2450e07f06e4bbf7e3bcee165b9e7326bbcfb978
Zhao Neng <zhaoneng@qutoutiao.net>
2019-02-23 17:15:04 +0800

    update

--- test.go
+++ test.go
@@ -1,8 +1,24 @@
 package testmod
 
-import "fmt"
+import (
+    "errors"
+    "fmt"
+)
 
-// Hi 返回一个友好的问候
-func Hi(name string) string {
-   return fmt.Sprintf("Hi, %s !!!", name)
+// Hi 返回一个欢迎语，其语言由 lang 指定
+func Hi(name, lang string) (string, error) {
+    switch lang {
+    case "en":
+        return fmt.Sprintf("Hi, %s!", name), nil
+    case "pt":
+        return fmt.Sprintf("Oi, %s!", name), nil
+    case "es":
+        return fmt.Sprintf("¡Hola, %s!", name), nil
+    case "fr":
+        return fmt.Sprintf("Bonjour, %s!", name), nil
+    case "cn":
+        return fmt.Sprintf("你好，%s！", name), nil
+    default:
+        return "", errors.New("unknown language")
+    }
 }
